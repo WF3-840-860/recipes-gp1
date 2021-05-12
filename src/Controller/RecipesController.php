949055376c68466bff09177846caf03f5b55993f
marie-ch <meryem.chariff@gmail.com>
2021-05-12 14:15:36 +0200

    ajout de la fonction addEdit()

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -27,64 +27,19 @@ class RecipesController extends AbstractController
             'articles' => $repository->findAll(),
         ]);
     }
+    
 
     /**
      * @Route("/recipes/add", name="recipes_add")
      */
     public function add(EntityManagerInterface $em): Response
     {
-        if (!empty($_POST)) {
-            // dd($_POST);
-            $post = array_map('trim', array_map('strip_tags', $_POST));
+        if(!empty ($_POST)){
 
-            /* foreach($_POST as $key => $value){
-                $post[$key] = trim(strip_tags($value));  
-            }*/
+            $article = new Articles();  // Je prépare l'instance de classe (donc un objet)
 
-            if (
-                !empty($post['title']) && strlen($post['title']) > 5 &&
-                !empty($post['content']) && strlen($post['content']) > 5 &&
-                !empty($post['ingredient']) && strlen($post['ingredient']) > 5 &&
-                !empty($post['duration']) && is_numeric($post['duration']) && $post['duration'] > 0 &&
-                !empty($post['nb_person']) && is_numeric($post['nb_person'])  && $post['nb_person'] > 0
-            ) {
-
-                $tmp_name = $_FILES["recipe_image"]["tmp_name"];
-                // verification de fichier
-                // TODO
-                $fichier =  uniqid() . '.jpg' ;
-                // . pathinfo($image, PATHINFO_EXTENSION)
-
-                // On va copier le fichier dans le dossier upload
-                $newfile = $this->getParameter('images_directory'). "/"  . $fichier;
-                move_uploaded_file($tmp_name, $newfile);
-                // $newfile = $this->getParameter('images_directory'). "/" .$fichier;
-               
-
-                // Equivalent du new PDO('mysql:....'); ou du manager
-                $entityManager = $this->getDoctrine()->getManager();
-
-                // Equivalent de la requete INSERT INTO
-                $recipe = new Articles();
-                $recipe->setTitle($post['title']); // Equivalent d'un bindParam()
-                $recipe->setContent($post['content']);
-                $recipe->setIngredient($post['ingredient']);
-                $recipe->setCreatedAt(new DateTime());
-                $recipe->setDuration($post['duration']);
-                $recipe->setNbPerson($post['nb_person']);
-                $recipe->setRecipeImage($fichier);
-
-                // Equivalent prepare()
-                $entityManager->persist($recipe);
-
-                // equivalent du execute() ou exec()
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Recette postée avec succes!');
-                return $this->redirectToRoute('recipes_index');
-            } else {
-                $this->addFlash('warning', 'Veuillez resaissir les champs !');
-            }
+            $this->addEdit($em, $article);
+            return $this->redirectToRoute('recipes_index');
         }
 
         return $this->render('recipes/add.html.twig');
@@ -102,39 +57,22 @@ class RecipesController extends AbstractController
         ]);
     }
 
-
-
-
-
-
-
     /**
      * @Route("/recipes/edit/{id}", name="recipes_edit", methods={"POST","GET"} )
      */
     public function edit(EntityManagerInterface $em, int $id): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        $article = $em->getRepository(Articles::class)->find($id);
+        $article = $em->getRepository(Articles::class)->find($id); // Je récupère mon instance de classe via l'article trouvé
 
         if (!$article) {
             throw $this->createNotFoundException(
                 'No product found for id ' . $id
             );
         }
-        if (!empty($_POST)) {
-            $post = array_map('trim', array_map('strip_tags', $_POST));
+        if(!empty ($_POST)){
 
-
-            $article->setTitle($post['title']);
-            $article->setContent($post['content']);
-            $article->setIngredient($post['ingredient']);
-            $article->setCreatedAt(new \DateTime());
-            $article->setDuration($post['duration']);
-            $article->setNbPerson($post['nb_person']);
-
-            $em->flush();
-
-            $this->addFlash('success', 'Modification effectuée');
+         
+            $this->addEdit($em, $article);
             return $this->redirectToRoute('recipes_show', [
             'id' => $article->getId(),
             'article' => $article
@@ -144,5 +82,57 @@ class RecipesController extends AbstractController
             'id' => $article->getId(),
             'article' => $article
         ]);
+    }
+
+    private function addEdit(EntityManagerInterface $em, Articles $article) :void
+    {
+       
+        $em = $this->getDoctrine()->getManager();
+      
+        if (!empty($_POST)) {
+            $post = array_map('trim', array_map('strip_tags', $_POST));
+            if (!empty($_FILES)) {
+                 $tmp_name = $_FILES["recipe_image"]["tmp_name"];
+            // verification de fichier
+            // TODO
+                 $fichier =  uniqid() . '.jpg' ;
+            // . pathinfo($image, PATHINFO_EXTENSION)
+
+            // On va copier le fichier dans le dossier upload
+                $newfile = $this->getParameter('images_directory'). "/"  . $fichier;
+                 move_uploaded_file($tmp_name, $newfile);
+            }else{
+              
+
+
+            }
+            $article->setTitle($post['title']);
+            $article->setContent($post['content']);
+            $article->setIngredient($post['ingredient']);
+            $article->setCreatedAt(new \DateTime());
+            $article->setDuration($post['duration']);
+            $article->setNbPerson($post['nb_person']);
+
+            // Update de l'image que si elle existe ou si elle est différente
+            if(!empty($article->getRecipeImage()) && $article->getRecipeImage() != $fichier){
+                $article->setRecipeImage($fichier);
+            }
+            elseif(empty($article->getRecipeImage())){ // Ajout
+                $article->setRecipeImage($fichier);
+            }
+
+            // ajout 
+            if(empty($article->getId())){
+                $em->persist($article);
+            }
+            
+
+            $em->flush();
+
+            $this->addFlash('success', 'Modification effectuée');
+        }else {
+            $this->addFlash('warning', 'Veuillez ressaisir les champs !');
+        }
+
     }
 }
