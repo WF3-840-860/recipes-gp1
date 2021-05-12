2d89ea6f28abff30bcc7dc27f47b74ed272ed803
SophieGodard <godard.sop@gmail.com>
2021-05-12 15:08:12 +0200

    add check for file input

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -86,26 +86,54 @@ class RecipesController extends AbstractController
 
     private function addEdit(EntityManagerInterface $em, Articles $article) :void
     {
-       
         $em = $this->getDoctrine()->getManager();
       
         if (!empty($_POST)) {
+
             $post = array_map('trim', array_map('strip_tags', $_POST));
+
+            // cas add 
             if (!empty($_FILES)) {
-                 $tmp_name = $_FILES["recipe_image"]["tmp_name"];
-            // verification de fichier
-            // TODO
-                 $fichier =  uniqid() . '.jpg' ;
-            // . pathinfo($image, PATHINFO_EXTENSION)
+                // dd($_FILES);
+                $target_dir = $this->getParameter('images_directory'). "/" ; // uploads directory
+                $file = basename($_FILES['recipe_image']['name']);
+                $target_file = $target_dir .$file;
+                $max_size = 5242880;
+                $size = $_FILES['recipe_image']['size'];
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                $extensions = array('png', 'gif', 'jpg', 'jpeg');
+                $tmp_name = $_FILES["recipe_image"]["tmp_name"];
+                
+                // verification de fichier
+                if (!in_array($imageFileType, $extensions)){
+                    throw $this->createNotFoundException(
+                        "l'extension du fichier n'est pas reconnu ['png', 'gif', 'jpg', 'jpeg']" . $file
+                    );
+                }
+                if ($size > $max_size){
+                    throw $this->createNotFoundException(
+                        'La taille du fichier dépasse la taille maxi ' . $max_size
+                    );
+                }
+
+                // Génère un identifiant unique
+                $fichier =  uniqid() . $imageFileType ;
 
             // On va copier le fichier dans le dossier upload
-                $newfile = $this->getParameter('images_directory'). "/"  . $fichier;
+                $newfile = $target_dir . $fichier;
                  move_uploaded_file($tmp_name, $newfile);
-            }else{
-              
+            }else 
+            {
+                //   cas edit
+                if ($article->getRecipeImage()){
+                    $fichier = $article->getRecipeImage();
 
+                    // 
+                }
+                
 
             }
+
             $article->setTitle($post['title']);
             $article->setContent($post['content']);
             $article->setIngredient($post['ingredient']);
