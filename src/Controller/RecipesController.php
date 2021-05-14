f2d5d31f894b5cc1b426d04bde3fd63f5ec97675
claire93-09 <clairejanet93@gmail.com>
2021-05-14 08:59:13 +0200

    message erreur pour verification mis a jour

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -196,6 +196,46 @@ class RecipesController extends AbstractController
         if (strlen($post['title']) < 5){
             $errors[] = 'La longueur du champ Titre est invalide';
         }
+        //On vérifie que le champs content est rempli
+        if (empty($post['content'])) {
+            $errors[] = ' Le champ Contenu est invalide';
+        }
+        //On vérifie que le champs content a longueur minimum de 5 caractères
+        if (strlen($post['content']) < 5){
+            $errors[] = 'La longueur du champ Contenu est invalide';
+        }
+        //On vérifie que le champs ingredient est rempli
+        if (empty($post['ingredient'])) {
+            $errors[] = ' Le champ Ingredient est invalide';
+        }
+        //On vérifie que le champs ingredient a longueur minimum de 5 caractères
+        if (strlen($post['ingredient']) < 5){
+            $errors[] = 'La longueur du champ Ingredient est invalide';
+        }
+        //On vérifie que le champs duration est rempli
+        if (empty($post['duration'])) {
+            $errors[] = ' Le champ Duree est invalide';
+        }
+        //On vérifie que le champs duration est strictement superieur a 0
+        if (is_numeric($post['duration']) >0){
+            $errors[] = 'La longueur du champ Duree est invalide';
+        }
+        //On vérifie que le champs duration est un entier
+        if (empty($post['duration'])) {
+            $errors[] = ' Le champ Duree doit etre un chiffre superieur a 0';
+        }
+        //On vérifie que le champs nb_person est rempli
+        if (empty($post['nb_person'])) {
+            $errors[] = ' Le champ Nombre de personne est invalide';
+        }
+        //On vérifie que le champs nb_person est strictement superieur a 0
+        if (is_numeric($post['nb_person']) >0){
+            $errors[] = 'La longueur du champ Nombre de personne est invalide';
+        }
+        //On vérifie que le champs nb_person est un entier
+        if (empty($post['nb_person'])) {
+            $errors[] = ' Le champ Nombre de personne doit etre un chiffre superieur a 0';
+        }
 
         // !empty($post['content']) && strlen($post['content']) > 5 &&
         // !empty($post['ingredient']) && strlen($post['ingredient']) > 5 &&
