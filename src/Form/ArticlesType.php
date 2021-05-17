74b03a0686c0a87ce8594c6781e3caa9ba2bc739
SophieGodard <godard.sop@gmail.com>
2021-05-17 13:41:38 +0200

    add link to connexion

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('ingredient')
+            ->add('created_at')
+            ->add('duration')
+            ->add('nb_person')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
